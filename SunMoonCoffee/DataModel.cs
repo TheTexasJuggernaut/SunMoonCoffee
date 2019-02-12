cb43eec66d385796ead011a3826df99b223793db
shawn.p.alexander <shawn.p.alexander@nasa.gov>
2019-02-11 20:07:21 -0600

    Fixed

--- SunMoonCoffee/DataModel.cs
+++ SunMoonCoffee/DataModel.cs
@@ -6,7 +6,7 @@ using System.Web;
 namespace SunMoonCoffee
 {
     public class DataModel
-    { //Update
+    { //Up 
 
         public Dictionary<string, string> coffeeItems = new Dictionary<string, string>() {
             { "Coffee", "Sun & Moon Coffee In House Brew" },
